f7f3e1710fcd672ef1bbcf537c73180aca2767e3
AdzqiAulia <72446013+AdzqiAulia@users.noreply.github.com>
2022-08-26 22:58:43 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,345 @@
+ <!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <title>aulibeauty_id e HTML Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!--
+TemplateMo 546 Sixteen Clothing
+https://templatemo.com/tm-546-sixteen-clothing
+-->
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-sixteen.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.html"><h2>auli <em>beauty_id</em></h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.html">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li> 
+              <li class="nav-item">
+                <a class="nav-link" href="products.html">Produk</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">Tentang</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Kontak</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="promo.html">Promo</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Page Content -->
+    <!-- Banner Starts Here -->
+    <div class="banner header-text">
+      <div class="owl-banner owl-carousel">
+        <div class="banner-item-01">
+          <div class="text-content">
+            <h2>Welcome To Aulibeauty_id</h2>
+            <h4>Skincare & Bodycare </h4>
+          </div>
+        </div>
+        <div class="banner-item-02">
+          <div class="text-content">
+            <h2>Welcome To Aulibeauty_id</h2>
+            <h4>Skincare & Bodycare </h4>
+          </div>
+        </div>
+        <div class="banner-item-03">
+          <div class="text-content">
+            <h2>Welcome To Aulibeauty_id</h2>
+            <h4>Skincare & Bodycare </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Banner Ends Here -->
+
+    <div class="latest-products">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Latest Products</h2>
+              <a href="products.html">view all products <i class="fa fa-angle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/ohmyskin.official_body.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>OhMySkin </h4></a>
+                <h6>Rp. 75.000</h6>
+                <p>Lation Spray</p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (845)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/ohmyskin.masker.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>OhMySkin </h4></a>
+                <h6>Rp. 40.000</h6>
+                <p>Masker Organik 4 Varian </p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (890)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/ohmyskin.gloss.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>OhMySkin </h4></a>
+                <h6>Rp. 70.000</h6>
+                <p>Lip Serum</p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (542)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/makeoverid_CGHszktHZoS.jpg " alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Make Over </h4></a>
+                <h6>Rp.115.000</h6>
+                <p>Cliquematte Lip Stylo</p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li> 
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (759)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/ohmyskin.asses.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>OhMySkin </h4></a>
+                <h6>Rp. 80.000</h6>
+                <p>Essence ance & Lething</p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (460)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/runaskin_CDfTnYTpCSJ.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>RunaSkin</h4></a>
+                <h6>Rp. 25.000</h6>
+                <p> Masker Peel - Off </p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (689)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/holyglow.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Holyglow e</h4></a>
+                <h6>Rp. 10.000</h6>
+                <p>Masker Peel - Off <br> macam - macam varian</p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (653)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/edelia.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Edelia </h4></a>
+                <h6>Rp. 12.500</h6>
+                <p>masker organic <br>Reguler & Premium </p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (594)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/poupeepou (1).jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>poupeepou </h4></a>
+                <h6>Rp.10.000</h6>
+                <p>masker organik <br> Macam - macam varian</p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (871)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="best-features">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Tentang Owner</h2>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="left-content">
+              <h4>Adzqi Aulia</h4>
+              <p><a rel="nofollow" href="https://templatemo.com/tm-546-sixteen-clothing" target="_parent"></a> “Hai, perkenalkan saya Adzqi Aulia, anak sulung dari 3 bersaudara asal dari Pesawaran Gedong Tataan, Lampung.
+                <br>saya adalah Mahasiswa Universitas Teknokrat Indonesia, Fakultas Teknik dan Ilmu Komputer Prodi S1 Sistem Informasi. Selain saya menjadi mahasiswa, saya juga owner dari aulibeauty_id yaitu bisnis berkategori perawatan/kecantikan. Meskipun seorang mahasiswa bukan berarti tidak bisa berbisnis, sedikit cerita awal saya membuka bisnis ini bermodalkan niat dan uang jajan yang setiap harinya saya sisihkan,total yang saya dapat Rp. 200.000
+                <br>Awal saya Promosi Produk melalui WhatsApp dan Instagram ternyata banyak respon baik untuk order dan akhirnya bisnis yang saya buka ini membuai hasil yang baik, saya pun semakin tekun dan terus mencoba mengembangkan berbisnis ini.
+                Hingga saat ini saya memiliki Anggota Reseller dan memiliki akun bisnis di Shopee agar lebih terjangkau oleh orang banyak.
+                <br>Dengan binis ini banyak hal yang saya dapat di luar perkuliahan seperti membagi waktu menjadi mahasiswa dan pembisnis, cara berkomunikasi yang baik dan ramah, dan masih banyak lagi.
+                <br><a rel="nofollow" href="https://templatemo.com/contact">Contact us</a></p>
+      
+              <a target="_blank" href="https://www.instagram.com/adzqiaulia_/" class="filled-button"><i class="icon-instagram"></i> Follow Me</a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="right-image">
+              <img src="assets/images/Adz.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="inner-content">
+              <p>Copyright &copy; aulibeauty_id</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/isotope.js"></script>
+    <script src="assets/js/accordions.js"></script>
+
+
+    <script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+
+
+  </body>
+
+</html>
